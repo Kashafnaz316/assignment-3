9ddd9ed9d5229d30d051c9aedd46b3f9b9547ad5
Kashafnaz316 <nkashaf316@gmail.com>
2026-01-18 02:54:34 -0800

    Add files via upload

---
+++ ASSIGNMENT 3.py
@@ -0,0 +1,22 @@
+ Area of circle
+pi=3.14
+r=float(input("Write value of radius"))
+AOC=pi*r*r
+print(AOC)
+
+# Area of Square
+side=float(input("Enter value of side"))
+AOS=side*side
+print(AOS)
+
+# Area of Rectangle
+lenght=float(input("Enter value of lenght"))
+width=float(input("Enter value of width"))
+AOR=lenght*width
+print(AOR)
+
+# Area of Triangle
+base=float(input("Enter value of base"))
+height=float(input("Enter value of height"))
+AOT=(base*height)/2
+print(AOT)
